5b9d4e46b387bc372d5cbba486518774ca691fb8
white-gyu <white_gyu@naver.com>
2022-12-09 22:15:35 +0900

    chore : init project

---
+++ src/main/java/com/penguin/coffeebeanserver/CoffeeBeanServerApplication.java
@@ -0,0 +1,13 @@
+package com.penguin.coffeebeanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBeanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeBeanServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penguin/coffeebeanserver/CoffeeBeanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguin.coffeebeanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBeanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
