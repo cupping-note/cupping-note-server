637cbe31a019f07f012785e6d25bbe78f6a7f80a
white-gyu <white_gyu@naver.com>
2023-02-15 23:47:36 +0900

    feat: kakao 사용자 정보 조회 위한 queryparam serialize 로직 구현

--- src/main/java/com/penguin/cuppingnote/user/dto/OAuthKakaoTokenRequest.java
+++ src/main/java/com/penguin/cuppingnote/user/dto/OAuthKakaoTokenRequest.java
@@ -1,8 +1,11 @@
 package com.penguin.cuppingnote.user.dto;
 
-import lombok.*;
+import feign.Param;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,4 +15,29 @@ public class OAuthKakaoTokenRequest {
     private String redirectUri;
     private String authorizationCode;
     private String clientSecret;
+
+    @Param("grant_type")
+    public String getGrantType() {
+        return grantType;
+    }
+
+    @Param("client_id")
+    public String getClientId() {
+        return clientId;
+    }
+
+    @Param("redirect_uri")
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    @Param("code")
+    public String getAuthorizationCode() {
+        return authorizationCode;
+    }
+
+    @Param("client_secret")
+    public String getClientSecret() {
+        return clientSecret;
+    }
 }
