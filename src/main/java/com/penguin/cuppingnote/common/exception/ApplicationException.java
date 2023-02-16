272663d320e92470d35e63e7cb2ff318c3cad6df
white-gyu <white_gyu@naver.com>
2023-02-17 00:23:02 +0900

    feat: 에러 처리 위한 runtime exception 상속한 부모 예외 클래스 생성

---
+++ src/main/java/com/penguin/cuppingnote/common/exception/ApplicationException.java
@@ -0,0 +1,15 @@
+package com.penguin.cuppingnote.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public abstract class ApplicationException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+
+    protected ApplicationException(final HttpStatus httpStatus, final String detail) {
+        super(detail);
+        this.httpStatus = httpStatus;
+    }
+}
