f431702e05addbc8691ab15e32c4fcb85597aeb8
white-gyu <white_gyu@naver.com>
2023-02-23 00:27:51 +0900

    fix: string decoder bean 삭제 <- bean으로 등록하면 api 응답값을 모두 string decoder로 역직렬화 시도하여 오류 발생

--- src/main/java/com/penguin/cuppingnote/common/exception/OpenFeignExceptionHandler.java
+++ src/main/java/com/penguin/cuppingnote/common/exception/OpenFeignExceptionHandler.java
@@ -4,22 +4,20 @@ import com.penguin.cuppingnote.common.exception.openfeign.OpenFeignException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import feign.codec.StringDecoder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class OpenFeignExceptionHandler implements ErrorDecoder {
-    private final StringDecoder stringDecoder;
+    private final StringDecoder stringDecoder = new StringDecoder();
 
     @Override
     public Exception decode(String methodKey, Response response) {
         try {
             return new OpenFeignException(stringDecoder.decode(response, String.class).toString());
         } catch (IOException e) {
-            throw new OpenFeignException(response.reason());
+            return new OpenFeignException(response.reason());
         }
     }
 }
