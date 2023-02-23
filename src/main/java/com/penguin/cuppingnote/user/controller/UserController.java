89c804fa1401ea3d08f9532488544ab9ba7224ce
white-gyu <white_gyu@naver.com>
2023-02-24 00:25:47 +0900

    feat: 카카오 로그인 API request body 유효성 검사 추가

--- src/main/java/com/penguin/cuppingnote/user/controller/UserController.java
+++ src/main/java/com/penguin/cuppingnote/user/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -17,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/kakao/login")
-    public ResponseEntity<UserLoginResponseDto> loginByKakao(@RequestBody UserKakaoLoginRequestDto userKakaoLoginRequestDto) {
+    public ResponseEntity<UserLoginResponseDto> loginByKakao(@Valid @RequestBody UserKakaoLoginRequestDto userKakaoLoginRequestDto) {
         return ResponseEntity.ok(
                 userService.loginByKakao(userKakaoLoginRequestDto)
         );
--- src/main/java/com/penguin/cuppingnote/user/dto/request/UserKakaoLoginRequestDto.java
+++ src/main/java/com/penguin/cuppingnote/user/dto/request/UserKakaoLoginRequestDto.java
@@ -4,9 +4,15 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserKakaoLoginRequestDto {
+
+    @NotBlank(message = "redirect uri가 입력되지 않았습니다.")
     private String redirectUri;
+
+    @NotBlank(message = "authorization code가 입력되지 않았습니다.")
     private String authorizationCode;
 }
