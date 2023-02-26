d02b8715899417e39f9f45a9074dbb9c138947aa
white-gyu <white_gyu@naver.com>
2023-02-26 23:40:59 +0900

    feat: objectmapper LocalDateTime data type 대응 위한 설정 추가

--- src/main/java/com/penguin/cuppingnote/config/JacksonConfig.java
+++ src/main/java/com/penguin/cuppingnote/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.penguin.cuppingnote.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule());
     }
 }
