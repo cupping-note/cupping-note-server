117cb1fa9733e15ccf8daa7f1142fd124fa056ce
white-gyu <white_gyu@naver.com>
2023-02-27 23:43:51 +0900

    feat: refresh token으로 session 조회 메서드 구현

--- src/main/java/com/penguin/cuppingnote/user/domain/session/SessionRepository.java
+++ src/main/java/com/penguin/cuppingnote/user/domain/session/SessionRepository.java
@@ -2,5 +2,8 @@ package com.penguin.cuppingnote.user.domain.session;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SessionRepository extends JpaRepository<Session, Long> {
+    Optional<Session> findByRefreshToken(String refreshToken);
 }
